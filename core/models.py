1ab8bb37c94f7d8d9297ae5dd8d945d4f700d29d
Kirill Andreev <Hizenberg228@,ail.ru>
2020-09-28 21:06:26 +0300

    Commit 1

--- core/models.py
+++ core/models.py
@@ -21,7 +21,7 @@ class Item(models.Model):
     discount_price = models.FloatField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(default='default.jpg', upload_to='items/')
+    image = models.ImageField(upload_to='items/', blank=True)
 
     def __str__(self):
         return self.title
