bc6fd865fc38f83d624f1c35373a0859ddfdfbc1
Alexander Andreev <1996sasha2507@mail.ru>
2020-09-28 15:33:58 +0300

    Style home page

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from core.models import Item, Category
 
 
 def item_list(request):
-    items = Item.objects.all()
+    items = Item.objects.all().order_by('category')
     categories = Category.objects.all()
     context = {
         'items': items,
