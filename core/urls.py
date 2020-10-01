f3f3564cb01d2aec4a9ef1f1b88c5d2357a75a94
Kirill Andreev <Hizenberg228@,ail.ru>
2020-10-01 20:28:23 +0300

    Add add_to_cart function in views

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from core.views import item_list, item_detail
+from core.views import item_list, item_detail, add_to_cart
 
 urlpatterns = [
     path('', item_list, name='home-page'),
+    path('add_to_cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('<str:category_slug>/<str:item_slug>/', item_detail, name='item-detail'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
-from core.models import Item, Category
+from core.models import Item, Category, OrderItem, Order
 
 
 def item_list(request):
@@ -21,4 +23,25 @@ def item_detail(request, item_slug, category_slug):
         'category': category
     }
     return render(request, 'core/item_detail.html', context)
+
+
+@login_required
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(item=item, user=request.user, ordered=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            return redirect(item.get_absolute_url())
+        else:
+            order.items.add(order_item)
+            return redirect(item.get_absolute_url())
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        return redirect(item.get_absolute_url())
 
