862f817abfd150bda18647e882cbe2b25951d711
Alexander Andreev <1996sasha2507@mail.ru>
2020-10-10 11:02:56 +0300

    Fix items in cart count bug

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,12 +4,12 @@ from .models import Order
 def product_count(request):
     if request.user.is_authenticated:
         result = 0
-        order = Order.objects.filter(user=request.user, ordered=False)
-        if order.exists():
+        if Order.objects.filter(user=request.user, ordered=False).exists():
+            order = Order.objects.filter(user=request.user, ordered=False)
             for item in order[0].items.all():
                 result += item.quantity
         return {'items_in_cart': result}
-
+    return {'items_in_cart': 0}
 
 
 
