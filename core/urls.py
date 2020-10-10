34160520320488ba6320740948a4b1ba3146f0f0
Kirill Andreev <Hizenberg228@,ail.ru>
2020-10-10 22:28:54 +0300

    Add filters on home page

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from core.views import item_list, item_detail, add_to_cart, cart_list, cart_dele
 
 urlpatterns = [
     path('', item_list, name='home-page'),
+    path('order_by/<str:ordering_obj>', item_list, name='order-by'),
     path('cart/', cart_list, name='cart-page'),
     path('delete_from_cart/<slug>/', cart_delete_item, name='delete-from-cart'),
     path('delete_from_cart_single_item/<slug>/', cart_delete_single_item, name='delete-from-cart-single-item'),
