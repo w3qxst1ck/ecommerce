13e6a562e0a1d548825936b42458ba072d72d270
Alexander Andreev <1996sasha2507@mail.ru>
2020-10-10 12:14:08 +0300

    Fix models

--- core/models.py
+++ core/models.py
@@ -79,7 +79,7 @@ class WishItem(models.Model):
     adding_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username}'
+        return f'{self.user.username} - {self.item.title}'
 
     class Meta:
         verbose_name = "Избранный товар"
